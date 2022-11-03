77f6b6a1b33e4dce4fc5c9163300c3c58f070182
suleyman1406 <dadasovsuleyman126@gmail.com>
2022-11-03 22:28:37 +0400

    object file added

---
+++ object.js
@@ -0,0 +1,23 @@
+let myObject = {
+  name: "Suleyman",
+  surname: "Dadashov",
+  age: 21,
+  students: ["Ali", "Fuad", "Senan"],
+  isTeacher: true,
+  for: "senin ucun",
+  while: "sene gore",
+  // myFirstStudent: {
+  //   name: "Rashad",
+  //   surname: "Khalilov",
+  // },
+};
+
+// if (myObject.myFirstStudent) {
+//   console.log(myObject.myFirstStudent.name);
+// }
+
+// if ({ salam: "salam" }) {
+//   console.log("var");
+// } else {
+//   console.log("yoxdu");
+// }
